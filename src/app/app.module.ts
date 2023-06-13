ab782638d3a698597f484506248df63ef72a9618
Rodri <enrod_mont@outlook.es>
2023-06-13 14:14:54 -0500

    Se elimino ng-select2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { NgSelect2Module } from 'ng-select2';
 import { ModalComponent } from './pages/modal/modal.component';
 import { FacturacionElectronicaComponent } from './pages/facturacion-electronica/facturacion-electronica.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
@@ -33,7 +32,6 @@ import { RegistroDeUsuariosComponentComponent } from './pages/usuarios/registro-
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    NgSelect2Module,
     NgxPaginationModule
   ],
   providers: [],
